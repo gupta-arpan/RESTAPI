3a499a786faedb3976bca68c60063019dcc91319
Arpan Gupta <arpan@pop-os.localdomain>
2023-12-18 00:51:20 +0530

    refactored updateStudentQuery

--- src/student/queries.js
+++ src/student/queries.js
@@ -3,6 +3,6 @@ const getStudentByIdQuery = "SELECT * FROM students WHERE id = $1";
 const checkEmailExistQuery = "SELECT s FROM students s WHERE s.email = $1";
 const addStudentQuery = "INSERT INTO students(name, email, age, dob) VALUES ($1, $2, $3, $4)";
 const removeStudentQuery = "DELETE FROM students WHERE id = $1";
-const updateStudentQuery = "UPDATE students SET name = $1, email = $2, age = $3, dob = $4 WHERE id = $5";
+const updateStudentQuery = "UPDATE students SET name = COALESCE($1, name), email = COALESCE($2, email), age = COALESCE($3, age), dob = COALESCE($4, dob) WHERE id = $5";
 
 export { getStudentQuery, getStudentByIdQuery, checkEmailExistQuery, addStudentQuery, removeStudentQuery, updateStudentQuery };
