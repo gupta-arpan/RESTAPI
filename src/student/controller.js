297fbb7e899782f72e0d32ed07144504dc7c48cb
Arpan Gupta <arpan@pop-os.localdomain>
2023-12-18 11:48:30 +0530

    async/await based api calling

--- src/student/controller.js
+++ src/student/controller.js
@@ -1,66 +1,82 @@
 import { pool } from "../../db.js";
 import { getStudentQuery, getStudentByIdQuery, checkEmailExistQuery, addStudentQuery, removeStudentQuery, updateStudentQuery} from "./queries.js";
 
-const getStudents = (req,res) => {
-    pool.query(getStudentQuery, (error,result) => {
-        if(error) throw error;
-        res.status(200).json(result.rows)
-    })
-}
-
-const getStudentById = (req,res) => {
-    const id = parseInt(req.params.id);
-    pool.query(getStudentByIdQuery, [id], (error,result) => {
-        if (error) throw error;
+const getStudents = async (req,res) => {
+    try{
+        const result = await pool.query(getStudentQuery);
         res.status(200).json(result.rows);
-    })
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).send("Error occured while processing the request");
+    }
 }
 
-const addStudent = (req,res) => {
-    pool.query(checkEmailExistQuery, [email], (error,result) => {
-        if(result.rows.length){
-            res.send("Email already exists")
-        }
-        else{
-            pool.query(addStudentQuery, [name, email, age, dob], (error,result) => {
-                if(error) throw error;
-                res.status(201).send("Student added successfully");
-            })
-        }
-    })
+const getStudentById = async (req,res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const result = await pool.query(getStudentByIdQuery, [id]);
+        res.status(200).json(result.rows);
+    }
+    catch (error){
+        console.log(error);
+        res.status(500).send("Error occured while procssing the request");
+    }
 }
 
-const removeStudent = (req,res) => {
-    const id = parseInt(req.params.id);
-    pool.query(getStudentByIdQuery, [id], (error,result) => {
+const addStudent = async (req,res) => {
+    try{
+        const {name, email, age, dob} = req.body;
+        const result = await pool.query(checkEmailExistQuery, [email]);
+        if(!result.rows.length){
+            const result = await pool.query(addStudentQuery, [name, email, age, dob]);
+            res.status(200).send("Student is successfully registered in database.");
+        }else{
+            res.status(500).send("Student already exist in database!");
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Error occured while processing the request!")
+    }
+}
+
+const removeStudent = async (req,res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const result = await pool.query(getStudentByIdQuery, [id]);
         const noStudentFound = !result.rows.length;
         if(noStudentFound){
-            res.send("Student does not exist in database!");
+            res.status(500).send("Student does not exist in database!");
+        }else{
+            const result = await pool.query(removeStudentQuery, [id]);
+            res.status(202).send("Student is removed successfully from database.")
         }
-        else{
-            pool.query(removeStudentQuery, [id], (error, result) => {
-                if (error) throw error;
-                res.status(202).send("Student removed successfully from database.");
-            })
-        }
-    })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Error occurred while processing the request!");
+    }
 }
 
-const updateStudent = (req,res) => {
-    const {name, email, age, dob} = req.body;
-    const id = parseInt(req.params.id);
-    pool.query(getStudentByIdQuery, [id], (error,result) => {
+const updateStudent = async (req,res) => {
+    try{
+        const {name, email, age, dob} = req.body;
+        const id = parseInt(req.params.id);
+        const result = await pool.query(getStudentByIdQuery, [id]);
         const noStudentFound = !result.rows.length;
         if(noStudentFound){
-            res.send("Student does not exist in database");
+            res.status(500).send("Student does not exist in database");
         }
         else{
-            pool.query(updateStudentQuery, [name, email, age, dob, id], (error,result) => {
-                if (error) throw error;
-                res.status(202).send("student data updated successfully");
-            })
+            const result = await pool.query(updateStudentQuery, [name, email, age, dob, id]);
+            res.status(200).send("student data updated successfully");
         }
-    })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send("Error occurred while processing the request!");
+    }
 }
 
 export {getStudents, getStudentById, addStudent, removeStudent, updateStudent};
